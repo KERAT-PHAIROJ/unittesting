8b50588e9f0a00556f1f25728ea919cdb2183455
KERAT-PHAIROJ <keratiphairoj@gmail.com>
2018-09-06 17:33:02 +0700

    add

---
+++ testinglab/tests/testcode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from listunit import count_unique
+
+class TestBuiltins(unittest.TestCase):
+
+
+    """Test some python built-in methods"""
+
+
+
+
+
+def test_boderline_cases(self):
+    list = ["1", "1"]
+
+    self.assertEqual(1, count_unique(list))
+
+def test_typical_case(self):
+    list = ["1", "2", "3"]
+    self.assertEqual(3, count_unique(list))
+
+
+def test_impossible_case(self):
+    list = [None]
+    self.assertEqual(0, count_unique(list))
+
+
+def test_extreme_case(self):
+    list = []
+    for i in range(0, 500000):
+        list.append(2)
+    self.assertEqual(1, count_unique(list))
